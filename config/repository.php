0fc979f28418f227634d37509aff4dc8cc8d3e4c
mohamedkhedr700 <M.khedr@rowaad.net>
2023-10-17 00:29:56 +0300

    Enhance

---
+++ config/repository.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Repositories
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the repositories that will be used by your application.
+    |
+    |
+    */
+
+    'repositories' => [],
+];
