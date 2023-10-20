f1102e6a38dd36a8e135c9c4cc70cf8662498336
mohamedkhedr700 <M.khedr@rowaad.net>
2023-10-20 14:32:05 +0300

    Enhance

---
+++ src/Traits/Utility/WithRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Raid\Core\Repository\Traits\Utility;
+
+use ICanBoogie\Inflections\tr;
+
+trait WithRepository
+{
+    /**
+     * Get a model.
+     */
+    public static function getModel(): ?string
+    {
+        return static::config('model');
+    }
+
+    /**
+     * Get a route service provider.
+     */
+    public static function getRouteServiceProvider(): ?string
+    {
+        return static::config('route_service_provider');
+    }
+}
